44f859345c32389180aeb8bb2920a457914e6956
mrsi13nt <androgeme0@gmail.com>
2024-05-04 03:23:40 +0300

    little updates

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+// popup images
+
 function openPopup(imageUrl) {
     document.getElementById('popup-image').src = imageUrl;
     document.getElementById('popup').style.display = 'block';
@@ -7,6 +9,36 @@ function closePopup() {
     document.getElementById('popup').style.display = 'none';
 }
 
+
+// dark mode
+
+function colors(check){
+    const head_text = document.getElementById('main-head')
+    const table = document.getElementById('options-contain')
+    const sql = document.getElementById('sql')
+    const xss = document.getElementById('xss')
+    const hh = document.getElementById('hh')
+    const js = document.getElementById('js')
+    const footer = document.getElementById('foot') 
+    if (check == true){
+        head_text.style.color = "white"
+        table.style.backgroundColor = "#4b4a4a"
+        sql.style.backgroundColor = "#4b4a4a"
+        xss.style.backgroundColor = "#4b4a4a"
+        hh.style.backgroundColor = "#4b4a4a"
+        js.style.backgroundColor = "#4b4a4a"
+        footer.style.backgroundColor = "#383737"
+    }else{
+        head_text.style.color = "black"
+        table.style.backgroundColor = "#ffffff"
+        sql.style.backgroundColor = "#ffffff"
+        xss.style.backgroundColor = "#ffffff"
+        hh.style.backgroundColor = "#ffffff"
+        js.style.backgroundColor = "#ffffff"
+        footer.style.backgroundColor = "#bdb9b9"
+    }
+    
+}
 
 const darkModeBtn = document.getElementById('dark-mode-btn');
 let isDarkMode = false; // Initial state
@@ -20,8 +52,13 @@ function updateDarkMode() {
     if (isDarkMode) {
         document.body.classList.add('dark-mode');
         darkModeBtn.textContent = 'Light Mode';
+        colors(true);
     } else {
         document.body.classList.remove('dark-mode');
         darkModeBtn.textContent = 'Dark Mode';
+        colors(false)
     }
 }
+
+
+// scroll up icon
